b3588d3fa22489653f27dab6e6f043736f59e86f
4gboframram <79847791+4gboframram@users.noreply.github.com>
2022-05-12 20:49:19 -0400

    Delete main.py

--- main.py
+++
@@ -1,9 +0,0 @@
-from rust_macro.util import tokenize_string, splitargs
-
-tokens = tokenize_string("'Hello, World', Hello There")
-
-args = splitargs(tokens, delimiter=',')
-
-assert len(args) == 2
-assert args[0][0].string == "'Hello, World'"
-assert [i.string for i in args[1]] == ['Hello', 'There']
